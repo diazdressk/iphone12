8638e34fe427562aca9e926eddc70ed511ace7cb
Adilet Abdurasulov <altpen1993@gmail.com>
2021-03-13 17:26:15 +0600

    Second commit, one changeble elem with one active class

--- script/script.js
+++ script/script.js
@@ -2,28 +2,50 @@ document.addEventListener('DOMContentLoaded', () => {// js начнет рабо
   //дожидается загрузки домДерева
   'use strict';//не позволит запускать код,если в нём есть ошибки(если есть необъявленные переменные и тд)
 
-  const tabs = () => {//табы,могу переключаться по объектам,которые внутри
-    const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');//псевдомассивы
-    // console.log(cardDetailChangeElems)
-    const cardDetailsTitleElems = document.querySelectorAll('.card-details__title');
-    const cardImageElems = document.querySelectorAll('.card__image');
+  const tabs = () => {
+    const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');//все элементы
+    const cardDetailsTitleElem = document.querySelector('.card-details__title');//один заголовок
+    const cardImageItemElem = document.querySelector('.card__image_item');
+    const cardDetailsPriceElem = document.querySelector('.card-details__price');
+    const descriptionMemory = document.querySelector('.description__memory');
 
-    const hideAll = () => {//функция удаления класса activ
-      for (let i = 0; i < cardDetailChangeElems.length; i += 1) {
-        cardDetailChangeElems[i].classList.remove('active');
-        cardDetailsTitleElems[i].classList.remove('active');
-        cardImageElems[i].classList.remove('active');
-      }
+    const data = [
+      {
+        name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
+        img: 'img/iPhone-graphite.png',
+        price: 29990,
+        memory: 128,
+      },
+      {
+        name: 'Смартфон Apple iPhone 12 Pro 512GB Silver',
+        img: 'img/iPhone-silver.png',
+        price: 32990,
+        memory: 512,
+      },
+      {
+        name: 'Смартфон Apple iPhone 12 Pro 768GB Pacific Blue',
+        img: 'img/iPhone-blue.png',
+        price: 35990,
+        memory: 768,
+      },
+    ];
+
+    const deactive = () => {
+      cardDetailChangeElems.forEach(btn => btn.classList.remove('active'));
     };
-    for (let i = 0; i < cardDetailChangeElems.length; i += 1) {
-      hideAll();
-      cardDetailChangeElems[i].addEventListener('click', () => {//функция добавления класса active
-        hideAll();
-        cardDetailChangeElems[i].classList.add('active');//добавляю класс active при нажатии
-        cardDetailsTitleElems[i].classList.add('active');
-        cardImageElems[i].classList.add('active');
+    cardDetailChangeElems.forEach((btn, i) => {
+      btn.addEventListener('click', () => {
+        if (!btn.classList.contains('active')) {//если btn не содержит класс active
+          deactive();
+          btn.classList.add('active');
+          cardDetailsTitleElem.textContent = data[i].name;
+          cardImageItemElem.src = data[i].img;//добавляю атрибуты
+          cardImageItemElem.alt = data[i].name;
+          cardDetailsPriceElem.textContent = data[i].price + '₽';
+          descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memory} ГБ`;
+        }
       });
-    }
+    })
   };
   tabs();
 });
